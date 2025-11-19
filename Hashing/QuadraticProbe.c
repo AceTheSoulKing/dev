726e41339d028a72fb02e7ac690ab9f847a625e4
AceTheSoulKing <tensazangetsu741@gmail.com>
2025-11-19 13:26:05 +0530

    Code for Quadratic Probing working properly h(x)=(h(x)+f(i))%size

---
+++ Hashing/QuadraticProbe.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#define size 10
+
+int _hash(int H[], int key)
+{
+    return key % size;
+}
+
+int _probe(int H[], int key)
+{
+    int index = _hash(H, key);
+    int i = 0;
+
+    while (H[(index + i * i) % size] != 0)
+        i++;
+
+    return (index + i * i) % size;
+}
+
+void _insert(int H[], int key)
+{
+    int index = _hash(H, key);
+
+    if (H[index] != 0)
+        index = _probe(H, key);
+    H[index] = key;
+}
+
+int _search(int H[], int key)
+{
+    int index = _hash(H, key);
+
+    int i = 0;
+    while (H[(index + i * i) % size] != key)
+        i++;
+    return (index + i * i) % size;
+}
+
+int main()
+{
+    int H[10] = {0};
+
+    _insert(H, 25);
+    _insert(H, 55);
+    _insert(H, 65);
+
+    int temp = _search(H, 55);
+    temp = _search(H, 100);
+}
